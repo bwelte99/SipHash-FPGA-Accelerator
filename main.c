a2fb37d14b41dd075ebca726bc4c81b51e48fc88
bwelte99 <71848340+bwelte99@users.noreply.github.com>
2022-09-04 20:22:30 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -18,6 +18,7 @@
 #include "platform.h"
 #include "xil_printf.h"
 #include "siphash.h"
+#include "addresses.h"
 //#include "vectors_papertest.h"
 //#include "vectors_7B.h"
 //#include "vectors_15B.h"
@@ -27,53 +28,23 @@
 //#include "vectors_255B.h"
 //#include "vectors_511B.h"
 //#include "vectors_1023B.h"
-#include "vectors_2047B.h"
+//#include "vectors_2047B.h"
 //#include "vectors_4095B.h"
 //#include "vectors_8191B.h"
 //#include "vectors_16383B.h"
 //#include "vectors_32767B.h"
 //#include "vectors_65535B.h"
 //#include "vectors_1048575B.h"
+#include "vectors_full.h"
 
-/*
- * DMA Register Map:
- * 	Base addr:	MM2S Control Reg
- * 	Base + 1:	MM2S Status Reg
- * 	...
- * 	Base + 6:	source addr[31:0]
- * 	Base + 7:	source addr[63:32]
- * 	...
- * 	base + 10: MM2S Transfer Length (bytes)
- */
-#define DMA_ADDR (uint32_t*) 0x40400000
-
-/*
- * Timer Register Map:
- * Base addr: 	Timer 0 Control/Status
- * Base + 1:	Timer 0 Load Reg
- * Base + 2:	Timer 0 Count Reg
- */
-#define TIMER_ADDR (uint32_t*) 0x42800000
-
-/*
- * SipHash Register Map:
- * 	Base addr: 	k0[31:0]
- * 	Base + 1:	k0[63:32]
- * 	Base + 2:	k1[31:0]
- * 	Base + 3:	k1[63:32]
- * 	Base + 4:	Soft Reset (active high, only 0th bit)
- * 	Base + 5:	Hash count
- * 	Base + 6:	Hash value [31:0]
- * 	Base + 7:	Hash value [63:32]
- */
-#define SIPHASH_ADDR (uint32_t*) 0x43C00000
+#define NUM_TEST_CORES 2
 
 int main()
 {
     init_platform();
 
     xil_printf("\n\n\r*******************************\n\r");
-    xil_printf("Starting %d-byte test ...\n\r", test_vector_size_bytes);
+    xil_printf("%d SipHash cores\n\r", NUM_TEST_CORES);
 
     uint32_t hw_start_time, hw_end_time, hw_hash_time = 0;
 
@@ -95,42 +66,50 @@ int main()
     //*******************
     //Program SipHash
     //*******************
-    //Reset SipHash core
-    *(SIPHASH_ADDR + 4) = 1;
-    *(SIPHASH_ADDR + 4) = 0;
+    for (int i = 0; i < NUM_TEST_CORES; i++){
+    	//Reset SipHash core
+    	*(SIPHASH_ADDR[i] + 4) = 1;
+    	*(SIPHASH_ADDR[i] + 4) = 0;
 
-    //Write k0 to registers 0 and 1
-    *(SIPHASH_ADDR) = keys[0] & 0xFFFFFFFF;
-    *(SIPHASH_ADDR + 1) = (keys[0] & 0xFFFFFFFF00000000) >> 32;
+    	//Write k0 to registers 0 and 1
+    	*(SIPHASH_ADDR[i]) = keys[0] & 0xFFFFFFFF;
+    	*(SIPHASH_ADDR[i] + 1) = (keys[0] & 0xFFFFFFFF00000000) >> 32;
 
-    //Write k1 to registers 2 and 3
-    *(SIPHASH_ADDR + 2) = keys[1] & 0xFFFFFFFF;
-    *(SIPHASH_ADDR + 3) = (keys[1] & 0xFFFFFFFF00000000) >> 32;
+    	//Write k1 to registers 2 and 3
+    	*(SIPHASH_ADDR[i] + 2) = keys[1] & 0xFFFFFFFF;
+    	*(SIPHASH_ADDR[i] + 3) = (keys[1] & 0xFFFFFFFF00000000) >> 32;
 
-    //Read keys back and print them to Putty
+    	//Read keys back and print them to Putty
 
-    uint32_t k0_read_low = *(SIPHASH_ADDR);
-    uint32_t k0_read_high = *(SIPHASH_ADDR + 1);
+    	uint32_t k0_read_low = *(SIPHASH_ADDR[i]);
+    	uint32_t k0_read_high = *(SIPHASH_ADDR[i] + 1);
 
-    uint32_t k1_read_low = *(SIPHASH_ADDR + 2);
-    uint32_t k1_read_high = *(SIPHASH_ADDR + 3);
+    	uint32_t k1_read_low = *(SIPHASH_ADDR[i] + 2);
+    	uint32_t k1_read_high = *(SIPHASH_ADDR[i] + 3);
 
-    xil_printf("Keys: \t%08X%08X\n\r", k0_read_high, k0_read_low);
-    xil_printf("\t%08X%08X\n\r", k1_read_high, k1_read_low);
+    	if (i == 0){
+        	xil_printf("SipHash Keys %d: \t%08X%08X\n\r", i, k0_read_high, k0_read_low);
+        	xil_printf("\t\t\t%08X%08X\n\r", k1_read_high, k1_read_low);
+    	}
+    }
 
 
     //******************
     //Program the DMA
     //******************
-    //Reset the DMA
-    *(DMA_ADDR) |= 0x4;
-    usleep(1000);
+    for (int i = 0; i < NUM_TEST_CORES; i++){
 
-    //Set Run = 1 in control reg
-    *(DMA_ADDR) |= 0x1;
+    	//Reset the DMA
+    	*(DMA_ADDR[i]) |= 0x4;
+    	usleep(1000);
 
-    //set src_addr = test_vectors_addr
-    *(DMA_ADDR + 6) = (uint32_t)test_vectors_addr;
+    	//Set Run = 1 in control reg
+    	*(DMA_ADDR[i]) |= 0x1;
+
+    	//set src_addr = test_vectors_addr
+    	*(DMA_ADDR[i] + 6) = (uint32_t)test_vectors_addr;
+
+    }
 
     //*******************
     //Configure the Timer
@@ -141,86 +120,167 @@ int main()
     //Configure the timer in cyclic, generate, count-up mode
     *(TIMER_ADDR) = 0x00000011;
 
-
+    /*
     //Sleep for 10 seconds so the ILA can be armed before the DMA runs
     xil_printf("\nSleeping for 10 s (arm the ILA now!)\n\r");
     sleep(10);
+    */
 
+    //run 15 times, collecting data for different vector lengths
+    int test_vector_size_bytes = 7;
+    int hash_index = 0;
+    uint32_t temp_hash;
 
-    //Start the timer
-    xil_printf("\nStarting Timer & DMA transaction...\n\r");
-    hw_start_time = *(TIMER_ADDR + 2);
-    *(TIMER_ADDR) |= 0x00000080;
+    while (test_vector_size_bytes <= 1048575){
 
-    //Write message length to DMA (should start the transaction)
-    //Note: this is one less than the exact size, but it will still
-    //		work bc the SipHash core ignores tkeep
-    *(DMA_ADDR + 10) = test_vector_size_bytes;
+		xil_printf("\n\r\t------\n\r");
+        xil_printf("\t%d-byte test\n\r", test_vector_size_bytes);
 
-    //Poll SipHash count reg & stop timer when count = 1
-    do {
-    	hw_count = *(SIPHASH_ADDR + 5);
+		//Start the timer
+		hw_start_time = *(TIMER_ADDR + 2);
+		*(TIMER_ADDR) |= 0x00000080;
+
+		//Write message length to DMA (should start the transaction)
+		//Note: this is one less than the exact size, but it will still
+		//		work bc the SipHash core ignores tkeep
+		for (int i = 0; i < NUM_TEST_CORES; i++){
+			*(DMA_ADDR[i] + 10) = test_vector_size_bytes;
+		}
+
+		//Incorrect way to check that will print the 13 hashes
+		/*
+		do {
+			temp_count += *(SIPHASH_ADDR[NUM_TEST_CORES - 1] + 5);
+		}
+		while(temp_count == hash_count);
+		*/
+
+		do{
+			temp_hash = *(SIPHASH_ADDR[NUM_TEST_CORES - 1] + 6);
+		}
+		while((0xFFFFFFFF & hashes[hash_index]) != temp_hash);
+
+		//Record timer value after hashing is done
+		hw_end_time = *(TIMER_ADDR + 2);
+
+		//reset hash count to match the temp count
+		hash_index++;
+
+		//Check the hash and make sure it's valid
+		int flag = 0;
+		//check 0th hash
+		uint32_t hash_lower = *(SIPHASH_ADDR[0] + 6);
+		uint32_t hash_upper = *(SIPHASH_ADDR[0] + 7);
+
+		//check that 1st through nth hashes match the 0th
+		for (int i = 1; i < NUM_TEST_CORES; i++){
+			if (hash_lower != *(SIPHASH_ADDR[i] + 6)){
+				flag = i;
+			}
+			else if (hash_upper != *(SIPHASH_ADDR[i] + 7)){
+				flag = i;
+			}
+		}
+
+		//Run software version of SipHash and store timer values before and after completion
+		sw_start_time = *(TIMER_ADDR + 2);
+		for (int i = 0; i < NUM_TEST_CORES; i++){
+			siphash(test_vectors, test_vector_size_bytes, keys, sw_hash_ptr, 8);
+		}
+		sw_end_time = *(TIMER_ADDR + 2);
+
+		//Print results to Putty
+
+		xil_printf("\tHardware Hash: %08X%08X\n\r", hash_upper, hash_lower);
+		sprintf(hw_hash_string, "%08lX%08lX", hash_upper, hash_lower);
+		if (flag){
+				xil_printf("\tWARNING: hash 0 and hash %d don't match!\n\r", flag);
+		}
+
+		xil_printf("\tSoftware Hash: ");
+
+		for (int i = 7; i >= 0; i--){
+			xil_printf("%02X", *(sw_hash_ptr + i));
+		}
+		xil_printf("\n\r");
+
+		sprintf(sw_hash_string, "%02X%02X%02X%02X%02X%02X%02X%02X", *(sw_hash_ptr + 7), *(sw_hash_ptr + 6), *(sw_hash_ptr + 5), *(sw_hash_ptr + 4), *(sw_hash_ptr + 3), *(sw_hash_ptr + 2), *(sw_hash_ptr + 1), *(sw_hash_ptr));
+
+		//int ok = ((sw_hash_lower == hw_hash_lower) && (sw_hash_upper == hw_hash_upper));
+		int ok = !strcmp(sw_hash_string, hw_hash_string);
+
+		if(ok){
+			xil_printf("\n\r\tThe hash matches :)\n\r");
+		}
+		else{
+			xil_printf("\tThe hash does not match :(\n\r");
+		}
+
+		//calculate hash times (accounting for overflow)
+		hw_hash_time = hw_end_time - hw_start_time;
+		if(hw_hash_time < 0){
+			hw_hash_time += 0xFFFFFFFF;
+		}
+
+		sw_hash_time = sw_end_time - sw_start_time;
+		if(sw_hash_time < 0){
+			sw_hash_time += 0xFFFFFFFF;
+		}
+
+		xil_printf("\tHardware Hashing Time: %d clock cycles\n\r", hw_hash_time);
+		xil_printf("\tSoftware hashing time: %d cycles", sw_hash_time);
+
+		switch(test_vector_size_bytes){
+			case(7):
+				test_vector_size_bytes = 15;
+				break;
+			case(15):
+				test_vector_size_bytes = 31;
+				break;
+			case(31):
+				test_vector_size_bytes = 63;
+				break;
+			case(63):
+				test_vector_size_bytes = 127;
+				break;
+			case(127):
+				test_vector_size_bytes = 255;
+				break;
+			case(255):
+				test_vector_size_bytes = 511;
+				break;
+			case(511):
+				test_vector_size_bytes = 1023;
+				break;
+			case(1023):
+				test_vector_size_bytes = 2047;
+				break;
+			case(2047):
+				test_vector_size_bytes = 4095;
+				break;
+			case(4095):
+				test_vector_size_bytes = 8191;
+				break;
+			case(8191):
+				test_vector_size_bytes = 16383;
+				break;
+			case(16383):
+				test_vector_size_bytes = 32767;
+				break;
+			case(32767):
+				test_vector_size_bytes = 65535;
+				break;
+			case(65535):
+				test_vector_size_bytes = 1048575;
+				break;
+			case(1048575):
+				test_vector_size_bytes = 1048576;
+				break;
+		}
     }
-    while(hw_count == 0);
-
-    //Record timer value after hashing is done
-    hw_end_time = *(TIMER_ADDR + 2);
-
-    xil_printf("Exiting polling loop ...\n\r");
-
-    //Check the hash and make sure it's valid
-    uint32_t hash_lower = *(SIPHASH_ADDR + 6);
-    uint32_t hash_upper = *(SIPHASH_ADDR + 7);
-
-    //Run software version of SipHash and store timer values before and after completion
-    sw_start_time = *(TIMER_ADDR + 2);
-    siphash(test_vectors, test_vector_size_bytes, keys, sw_hash_ptr, 8);
-    sw_end_time = *(TIMER_ADDR + 2);
-
-    //Print results to Putty
-    xil_printf("\n\r*******************************\n\r");
-    xil_printf("Results:\n\r");
-    xil_printf("*******************************\n\r");
-
-    xil_printf("Hardware Hash: %08X%08X\n\r", hash_upper, hash_lower);
-    sprintf(hw_hash_string, "%08lX%08lX", hash_upper, hash_lower);
-
-    xil_printf("Software Hash: ");
-
-    for (int i = 7; i >= 0; i--){
-    	xil_printf("%02X", *(sw_hash_ptr + i));
-    }
-    xil_printf("\n\r");
-
-    sprintf(sw_hash_string, "%02X%02X%02X%02X%02X%02X%02X%02X", *(sw_hash_ptr + 7), *(sw_hash_ptr + 6), *(sw_hash_ptr + 5), *(sw_hash_ptr + 4), *(sw_hash_ptr + 3), *(sw_hash_ptr + 2), *(sw_hash_ptr + 1), *(sw_hash_ptr));
-
-    //int ok = ((sw_hash_lower == hw_hash_lower) && (sw_hash_upper == hw_hash_upper));
-    int ok = !strcmp(sw_hash_string, hw_hash_string);
-
-    if(ok){
-    	xil_printf("\n\rThe hash matches :)\n\r");
-    }
-    else{
-    	xil_printf("The hash does not match :(\n\r");
-    }
-
-    //calculate hash times (accounting for overflow)
-    hw_hash_time = hw_end_time - hw_start_time;
-    if(hw_hash_time < 0){
-    	hw_hash_time += 0xFFFFFFFF;
-    }
-
-    sw_hash_time = sw_end_time - sw_start_time;
-    if(sw_hash_time < 0){
-    	sw_hash_time += 0xFFFFFFFF;
-    }
-
-    xil_printf("Hardware Hashing Time: %d clock cycles\n\r", hw_hash_time);
-
-    xil_printf("Software hashing time: %d cycles", sw_hash_time);
 
     free(sw_hash_ptr);
-
+    fflush(stdout);
     cleanup_platform();
     return 0;
 }
